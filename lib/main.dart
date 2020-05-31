672d52775a969449fbf849ef328f0bcd3fdd9b8c
Avinash <avinashfab5678@gmail.com>
2020-06-01 01:29:34 +0530

    completed upto list view and getting started with que display

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,12 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> ScoreKeeper = [
+    Icon(Icons.check, color: Colors.green),
+    Icon(Icons.close, color: Colors.red),
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -61,7 +67,11 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  ScoreKeeper.add(
+                    Icon(Icons.check, color: Colors.green),
+                  );
+                });
               },
             ),
           ),
@@ -79,12 +89,18 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  ScoreKeeper.add(
+                    Icon(Icons.close, color: Colors.red),
+                  );
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: ScoreKeeper,
+        )
       ],
     );
   }
